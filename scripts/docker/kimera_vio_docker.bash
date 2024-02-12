3e356d0231e3382a057f8db85553760470bba677
Chase Kunz <17326199+chasekunz@users.noreply.github.com>
2024-02-12 16:35:47 -0700

    Fix docker script for openGL, canberra-gtk

--- scripts/docker/kimera_vio_docker.bash
+++ scripts/docker/kimera_vio_docker.bash
@@ -6,6 +6,7 @@ docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="/dev/dri:/dev/dri" \
     kimera_vio
 # Disallow X server connection
 xhost -local:root
